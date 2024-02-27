1764c0e1d0f3982a0537edb575b5b7217551bff1
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-27 13:25:00 +0100

    Update RSS export and fix link in RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,7 +7,7 @@ const sortedPosts = Object.values(allPosts).sort(
 	(a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
 );
 
-export const get = () =>
+export const GET = () =>
 	rss({
 		// `<title>` field in output xml
 		title: `${SITE.name} | Blog`,
@@ -22,7 +22,7 @@ export const get = () =>
 		items: sortedPosts.map((item) => ({
 			title: item.data.title,
 			description: item.data.description,
-			link: `blog/${item.slug}`,
+			link: `all/${item.slug}`,
 			pubDate: new Date(item.data.date),
 		})),
 		// (optional) inject custom xml
